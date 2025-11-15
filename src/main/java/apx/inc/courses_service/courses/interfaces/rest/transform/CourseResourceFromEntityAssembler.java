6689ea5e7325641bf2d4d92d9a9d9f6b6a4d7b55
EricMOB-afk <U202315032@upc.edu.pe>
2025-11-15 02:05:12 -0500

    fix get all courses

--- src/main/java/apx/inc/courses_service/courses/interfaces/rest/transform/CourseResourceFromEntityAssembler.java
+++ src/main/java/apx/inc/courses_service/courses/interfaces/rest/transform/CourseResourceFromEntityAssembler.java
@@ -3,6 +3,8 @@ package apx.inc.courses_service.courses.interfaces.rest.transform;
 import apx.inc.courses_service.courses.domain.model.aggregates.Course;
 import apx.inc.courses_service.courses.interfaces.rest.resources.CourseResource;
 
+import java.util.Set;
+
 public class CourseResourceFromEntityAssembler {
     public static CourseResource toResourceFromEntity(Course entity) {
         return new CourseResource(
@@ -10,7 +12,8 @@ public class CourseResourceFromEntityAssembler {
                 entity.getTeacherId(),
                 entity.getTitle(),
                 entity.getImageUrl(),
-                entity.getCourseJoinCode() != null ? entity.getCourseJoinCode().key() : null  // ✅ Manejar null
+                entity.getCourseJoinCode() != null ? entity.getCourseJoinCode().key() : null,
+                entity.getStudentIds() != null ? Set.copyOf(entity.getStudentIds()) : Set.of()
         );
     }
 }
